a452f742ac52ffecbf7837caeb80ecf255390bcd
Abuemkeze Chu <abuemkeze@gmail.com>
2023-09-09 08:57:25 +0100

    initial commit

---
+++ profile-card/src/index.js
@@ -0,0 +1,10 @@
+import { React } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App() {
+  return <div className="card">h</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
