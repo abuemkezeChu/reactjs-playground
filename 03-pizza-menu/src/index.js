629bb066338f981cbfa341a70464efc32f2cd1b1
Abuemkeze Chu <abuemkeze@gmail.com>
2023-09-10 10:21:58 +0100

    pizza menu project complete

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -110,17 +110,19 @@ function Menu() {
 function Pizza({ pizzaObject }) {
   console.log(pizzaObject)
 
-  if (pizzaObject.soldOut) {
-    return null
-  }
+  // if (pizzaObject.soldOut) {
+  //   return null
+  // }
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObject.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObject.photoName} alt="spinage pizza"></img>
       <div>
-        <span>{pizzaObject.price}</span>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
+        <span>
+          {pizzaObject.soldOut ? 'SOLD OUT' : pizzaObject.price + '💲'}
+        </span>
       </div>
     </li>
   )
