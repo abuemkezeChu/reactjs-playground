941224d71d70cad1be68587e87af51b7106796b5
Abuemkeze Chu <abuemkeze@gmail.com>
2023-11-04 05:22:15 +0100

    implement tip-calculator and handle reset

--- tip-calculator/src/App.jsx
+++ tip-calculator/src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './index.css'
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h1>Content Goes Here</h1>
+      <TipCalculator />
     </div>
   )
 }
 
-export default App
+function TipCalculator() {
+  const [bill, setBill] = useState('')
+  const [percentage1, setPercentage1] = useState(0)
+  const [percentage2, setPercentage2] = useState(0)
+
+  const tip = bill * (((percentage1 + percentage2) / 2) * 100)
+
+  function handleReset() {
+    setBill('')
+    setPercentage1(0)
+    setPercentage2(0)
+  }
+  return <div></div>
+}
